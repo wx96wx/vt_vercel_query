b59e0271aebdfc3e305bd6e9cbe71260f22f60e8
wx96wx <wym@cloudsploit.eu.org>
2026-01-03 09:01:06 +0800

    change to en

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,12 @@ export default function Home() {
 
   return (
     <div style={{ padding: 20, maxWidth: 800, margin: "0 auto" }}>
-      <h1>VirusTotal Hash 查询</h1>
+      <h1>VirusTotal Hash query</h1>
 
       <input
         value={hash}
         onChange={e => setHash(e.target.value)}
-        placeholder="输入文件 hash"
+        placeholder="input file hash"
         style={{ width: "100%", padding: 8 }}
       />
 
@@ -30,17 +30,17 @@ export default function Home() {
 
       {result && !result.error && (
         <div style={{ marginTop: 20 }}>
-          <p><b>文件名：</b>{result.filename || "-"}</p>
-          <p><b>签名：</b>{result.signature || "-"}</p>
-          <p><b>检测率：</b>{result.malicious}/{result.total}</p>
+          <p><b>filename：</b>{result.filename || "-"}</p>
+          <p><b>signature：</b>{result.signature || "-"}</p>
+          <p><b>malicious：</b>{result.malicious}/{result.total}</p>
 
-          <p><b>恶意引擎：</b></p>
+          <p><b>engines：</b></p>
           <ul>
             {result.engines.map(e => <li key={e}>{e}</li>)}
           </ul>
 
           <a href={`/api/download/${hash}`} target="_blank">
-            下载文件（如 API 权限允许）
+            download
           </a>
         </div>
       )}
